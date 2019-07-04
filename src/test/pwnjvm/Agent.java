aead426e1d6cd713a18a387c841a523f28476063
Jiang Xintong <50687608+NilesJiang@users.noreply.github.com>
2019-07-04 15:26:52 +0800

    Update Agent.java

--- src/test/pwnjvm/Agent.java
+++ src/test/pwnjvm/Agent.java
@@ -1,18 +1,20 @@
 package test.pwnjvm;
 
+
+/*@Description:This agent uses Byte Buddy so that can easily attach new Classes to JVMs.  
+*利用Byte Buddy创建能够快速注入类到JVM中的premain()。
+*/
 import java.lang.instrument.Instrumentation;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-//import net.bytebuddy.agent.ByteBuddyAgent;
+
 
 
 public class Agent {
 	public Agent() {}
-	//是在agent中，将自己的类编程attach到jvm的父类，或者说子类
+	
 	public static void premain(String arg, Instrumentation instrumentation){
 		System.out.println("--Method premain() gots executed at this position--");
-		//ByteBuddyAgent.install();
-		MyServlet foo = new MyServlet();
 		new ByteBuddy()
 		  .redefine(MyServlet.class)
 		  .name(javax.servlet.http.HttpServlet.class.getName())
@@ -23,7 +25,7 @@ public class Agent {
 	
 	
 	public static void premain(String arg){
-		
+	//do nothing	
 	}
 }
 
