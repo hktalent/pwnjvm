0353e8035868ae49f4171c8f177465b189d91d96
Jiang Xintong <50687608+NilesJiang@users.noreply.github.com>
2019-07-04 15:33:11 +0800

    Update Attach.java

--- src/test/pwnjvm/Attach.java
+++ src/test/pwnjvm/Attach.java
@@ -8,18 +8,23 @@ import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
 
 /*
- *@Description: 将agent附加到制定的jvm 
- * Attach机制：提供一种jvm进程间通信的能力，能让一个进程传命令给另外一个进程，并让它执行内部的一些操作。
+ * @Description: 将agent附加到指定的JVM
+ * 注：Attach机制提供了一种JVM进程间通信的能力，能让一个进程传命令给另外一个进程，并让它执行内部的一些操作。
+ * Attach agent to JVM.
  */
+
 public class Attach {
+    
+    //需要手动输入Virtual Machine的pid值。Require manually input of pid value.
     public static void main(String[] args) throws AttachNotSupportedException,
         IOException, AgentLoadException, AgentInitializationException { 
     	
+        //定义agent Path，此处应注意按情况修改
         String agentPath = "D:\\work\\workspace\\myjar\\loaded.jar";
-        //定义agent Path
-        String vid = args[0]; 
-        VirtualMachine vm = VirtualMachine.attach(vid);
-        //触发attach pid的关键，Attaches to a Java virtual machine.
+        String pid = args[0]; 
+        VirtualMachine vm = VirtualMachine.attach(pid);
+        //附加到指定的JVM。Attaches to a Java virtual machine.
         vm.loadAgent(agentPath);
+            
     }
 }
